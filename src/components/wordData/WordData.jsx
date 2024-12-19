0fe2df88a207c6e5406021e16dc324a2c5c0e6d6
Andy Agyei-Dwumah <andydwumah@gmail.com>
2024-12-19 21:50:29 +0000

    Add responsiveness to Search section

--- src/components/wordData/WordData.jsx
+++ src/components/wordData/WordData.jsx
@@ -4,7 +4,6 @@ export default function WordData({ word, styles, results, generateKey }) {
       <div>
         <p className={[styles.capitaliseText, styles.searchWord].join(" ")}>
           {word}
-          &nbsp;
         </p>
       </div>
       <div>
