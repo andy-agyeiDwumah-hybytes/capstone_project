bb9f4a087f04ca93c5e1c7f3fa24c51b01025d10
Andy Agyei-Dwumah <andydwumah@gmail.com>
2024-12-20 00:58:13 +0000

    Improve upon code quality in Error component

--- src/components/error/Error.jsx
+++ src/components/error/Error.jsx
@@ -6,9 +6,14 @@ import styles from "./Error.module.css"
 export default function Error() {
   return (
     <div className={styles.errorWrapper}>
-      <p>
-        Page not found. <Link to="/" className={styles.errorLink}>Back to home?</Link>
-      </p>
+      <div>
+        <p className={styles.errorText}>
+          Page not found.&nbsp;
+          <Link to="/" className={styles.errorLink}>
+            Back to home?
+          </Link>
+        </p>
+      </div>
     </div>
   );
 }
