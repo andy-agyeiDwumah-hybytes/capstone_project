49719f7309a2ff181c11bd74cb53ebed983325c6
Andy Agyei-Dwumah <andydwumah@gmail.com>
2024-12-21 17:20:34 +0000

    Install jQuery and use useEffect to update h1 tag on component mount

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,9 +3,17 @@ import { Helmet } from "react-helmet-async";
 import {useNavigate } from "react-router"
 // Styles
 import styles from "./Home.module.css"
+import { useEffect } from "react";
+// jQuery
+import $ from "jquery"
 
 export default function Home() {
   const navigate = useNavigate()
+
+  // Update H1 text using jQuery
+  useEffect(() => {
+    $("#home").text("VocabVault")
+  })
 
   const handleClick = () => {
     navigate("login")
@@ -22,7 +30,7 @@ export default function Home() {
         className={styles.hero}
       >
         <div className={styles.textContentWrapper}>
-          <h1 id="home" className={styles.heading}>VocabVault</h1>
+          <h1 id="home" className={styles.heading}></h1>
           <p className={styles.para}>Join 1,200+ users improving their vocabulary today!</p>
           <div>
             <button
